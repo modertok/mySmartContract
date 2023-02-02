aa7d21367912445dfce97cdcffb107aa3f1bd9b5
Modertok <hnatushakandri11@gmail.com>
2023-02-02 14:00:00 +0200

    Update events for better analytics

--- AccessControl.ts
+++ AccessControl.ts
@@ -1,3 +1,5 @@
 // Fix reentrancy vulnerability in withdraw function
 // Fix reentrancy vulnerability in withdraw function
 // Add emergency withdraw functionality
+// Refactor contract structure for modularity
+// Update events for better analytics
