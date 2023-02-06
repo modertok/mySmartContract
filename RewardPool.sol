c6d1fa45dc4f0f4bf2115fad691eab75ba2265d6
Modertok <hnatushakandri11@gmail.com>
2023-02-06 14:00:00 +0200

    Add staking mechanism with reward distribution

--- RewardPool.sol
+++ RewardPool.sol
@@ -1,3 +1,5 @@
 // Refactor contract structure for modularity
 // Add ownership modifiers to contract
 // Refactor contract structure for modularity
+// Implement initial token contract
+// Add staking mechanism with reward distribution
