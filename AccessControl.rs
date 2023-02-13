e83919169d07e628452aa40fa073a960529917ec
Modertok <hnatushakandri11@gmail.com>
2023-02-13 14:00:00 +0200

    Write tests for staking contract

--- AccessControl.rs
+++ AccessControl.rs
@@ -1,3 +1,5 @@
 // Update events for better analytics
 // Add emergency withdraw functionality
 // Implement initial token contract
+// Update events for better analytics
+// Write tests for staking contract
