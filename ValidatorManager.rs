b21cf4f2ee5e38a15833c10835937b61f2ac69ab
Modertok <hnatushakandri11@gmail.com>
2023-02-19 14:00:00 +0200

    Write documentation for governance module

--- ValidatorManager.rs
+++ ValidatorManager.rs
@@ -2,3 +2,5 @@
 // Add emergency withdraw functionality
 // Implement initial token contract
 // Fix overflow bug in reward calculator
+// Write tests for staking contract
+// Implement slashing mechanism for validators
