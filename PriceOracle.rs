a1f85b7df2d149fda53ea241eba67e3adb95888c
Modertok <hnatushakandri11@gmail.com>
2023-03-11 14:00:00 +0200

    Implement slashing mechanism for validators

--- PriceOracle.rs
+++ PriceOracle.rs
@@ -2,3 +2,5 @@
 // Add emergency withdraw functionality
 // Update events for better analytics
 // Implement slashing mechanism for validators
+// Add ownership modifiers to contract
+// Integrate Oracle for price feed
