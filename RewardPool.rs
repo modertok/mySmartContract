61c8d958318de15a8276b1fa0b78276e33aa2dd3
Modertok <hnatushakandri11@gmail.com>
2023-03-26 14:00:00 +0200

    Implement initial token contract

--- RewardPool.rs
+++ RewardPool.rs
@@ -2,3 +2,5 @@
 // Integrate Oracle for price feed
 // Write tests for staking contract
 // Write tests for staking contract
+// Implement slashing mechanism for validators
+// Add emergency withdraw functionality
