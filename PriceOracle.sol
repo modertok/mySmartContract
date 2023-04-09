a7ba20310105105dccd9b6385d385f580e0961d1
Modertok <hnatushakandri11@gmail.com>
2023-04-09 14:00:00 +0200

    Add staking mechanism with reward distribution

--- PriceOracle.sol
+++ PriceOracle.sol
@@ -7,3 +7,5 @@
 // Add governance proposal voting contract
 // Refactor contract structure for modularity
 // Add ownership modifiers to contract
+// Enhance access control with roles
+// Add ownership modifiers to contract
