6ca241c54abd3b26fefeaf67a2c3816cf87b06a0
Modertok <hnatushakandri11@gmail.com>
2023-05-11 14:00:00 +0200

    Update events for better analytics

--- ValidatorManager.sol
+++ ValidatorManager.sol
@@ -2,3 +2,5 @@
 // Add staking mechanism with reward distribution
 // Update events for better analytics
 // Add emergency withdraw functionality
+// Enhance access control with roles
+// Implement initial token contract
