562c3c8ce0c23bd798832ff56db21136f1ffeeeb
Modertok <hnatushakandri11@gmail.com>
2023-05-27 14:00:00 +0200

    Update events for better analytics

--- BatchTransferModule.rs
+++ BatchTransferModule.rs
@@ -2,3 +2,5 @@
 // Refactor contract structure for modularity
 // Add governance proposal voting contract
 // Integrate Oracle for price feed
+// Update events for better analytics
+// Enhance access control with roles
