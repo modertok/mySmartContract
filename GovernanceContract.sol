1da651db246a1cdca2059feb373316ed89b1ff13
Modertok <hnatushakandri11@gmail.com>
2023-07-18 14:00:00 +0200

    Update events for better analytics

--- GovernanceContract.sol
+++ GovernanceContract.sol
@@ -3,3 +3,5 @@
 // Implement slashing mechanism for validators
 // Enhance access control with roles
 // Fix reentrancy vulnerability in withdraw function
+// Write documentation for governance module
+// Fix overflow bug in reward calculator
