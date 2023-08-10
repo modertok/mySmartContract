bf1494adedb4250b0f3f0b5e4947ed7b8c135070
Modertok <hnatushakandri11@gmail.com>
2023-08-10 14:00:00 +0200

    Write documentation for governance module

--- TokenContract.ts
+++ TokenContract.ts
@@ -4,3 +4,5 @@
 // Enhance access control with roles
 // Write documentation for governance module
 // Update events for better analytics
+// Refactor contract structure for modularity
+// Refactor contract structure for modularity
