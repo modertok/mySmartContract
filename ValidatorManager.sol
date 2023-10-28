4e2a76382bca8d542516e23b445775947631ab7c
Modertok <hnatushakandri11@gmail.com>
2023-10-28 14:00:00 +0200

    Write tests for staking contract

--- ValidatorManager.sol
+++ ValidatorManager.sol
@@ -27,3 +27,4 @@
 // Fix reentrancy vulnerability in withdraw function
 // Enhance access control with roles
 // Implement slashing mechanism for validators
+// Fix overflow bug in reward calculator
