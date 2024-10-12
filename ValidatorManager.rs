7ed45f55735bdf078d02ec3d4e24ad564a7f4a45
Modertok <hnatushakandri11@gmail.com>
2024-10-12 14:00:00 +0200

    Add governance proposal voting contract

--- ValidatorManager.rs
+++ ValidatorManager.rs
@@ -50,3 +50,5 @@
 // Add governance proposal voting contract
 // Integrate Oracle for price feed
 // Enhance access control with roles
+// Implement slashing mechanism for validators
+// Add governance proposal voting contract
